617f7b3a3f958253f814747443483e594cf23df5
Connor Sullivan <cksullvn@gmail.com>
2019-09-24 19:44:25 -0500

    linked up cheese module

---
+++ javascripts/main.js
@@ -0,0 +1,10 @@
+import cheese from './components/cheese.js'
+//3rd thing: make init function in the main.js
+
+const init = () => {
+    //do stuff
+    cheese.printCheeseOptions();
+    //inside store there's a function call this and run it
+};
+
+init();
